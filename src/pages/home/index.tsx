b83db5913128af1e9fb247a66bc88b306dc94f96
xianxiangjin <xianxiang.jin@innodealing.com>
2024-06-18 17:21:00 +0800

    first ommit project to repo

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import { Button } from "antd";
+
+const HomePage = () => {
+  return (
+    <div style={{ height: "100%" }}>
+      <div>这是首页</div>
+      <Button type="primary">Antd 按钮</Button>
+      <p>好的， 哈哈哈....</p>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/pages/testpage/index.tsx
@@ -0,0 +1,9 @@
+const TestPage = () => {
+  return (
+    <div style={{ height: "100%" }}>
+      <div>这是测试页面</div>
+    </div>
+  );
+};
+
+export default TestPage;
